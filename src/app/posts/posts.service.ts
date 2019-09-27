9eaa2f165b08353541268bcbb8aa1395d9790257
Kathleen Ohde <Kathleen.Ohde1@t-mobile.com>
2019-09-27 15:59:58 -0500

    post service

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,5 +1,7 @@
 import { Post } from './post.model';
+import { Injectable } from '@angular/core';
 
+@Injectable({providedIn: 'root'})
 export class PostsService {
   private posts: Post[] = [];
 
@@ -9,6 +11,7 @@ export class PostsService {
 
   addPost(title: string, content: string) {
     const post: Post = {title: title, content: content};
+    console.log(post);
     this.posts.push(post);
   }
 }
